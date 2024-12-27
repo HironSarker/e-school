36adc13287afe86f0e02bd602053cc7bda58a6c9
unknown <hironsarker0005@gmail.com>
2024-12-27 16:39:02 +0600

    added SignUp Component

---
+++ e-school-client/src/Pages/SignUp/SignUp.jsx
@@ -0,0 +1,149 @@
+import { useForm } from "react-hook-form";
+import { toast } from "react-hot-toast";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import useAuth from "../../Hooks/useAuth/useAuth";
+import SocialLogin from "../Shared/SocialLogin/SocialLogin";
+
+const SignUp = () => {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    reset,
+  } = useForm();
+  const { createUser, updateUser } = useAuth();
+
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const from = location.state?.from?.pathname || "/";
+
+  const handleSignUp = (data) => {
+    data.photo = null;
+    const { email, name, password, role } = data || {};
+
+    createUser(email, password)
+      .then(() => {
+        updateUser(name)
+          .then(() => {
+            const saveUser = { email, name, role };
+
+            fetch(`${import.meta.env.VITE_API_URL}/users`, {
+              method: "POST",
+              headers: {
+                "content-type": "application/json",
+              },
+              body: JSON.stringify(saveUser),
+            })
+              .then((res) => res.json())
+              .then((data) => {
+                if (data.insertedID) {
+                  reset();
+                  toast.success("Successfully registration done");
+                  navigate(from, { replace: true });
+                }
+              });
+          })
+          .catch((error) => {
+            toast.error(error.message);
+          });
+      })
+      .catch((error) => {
+        toast.error(error.message);
+      });
+  };
+
+  return (
+    <div className="h-[500px] flex justify-center items-center">
+      <div className="w-96 p-7">
+        <h2 className="text-4xl text-center">Sign Up</h2>
+        <form onSubmit={handleSubmit(handleSignUp)}>
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              <span className="label-text">Name</span>
+            </label>
+            <input
+              type="text"
+              {...register("name", {
+                required: "Name is required",
+              })}
+              className="input input-bordered w-full max-w-xs"
+            />
+            {errors.name && (
+              <p className="text-red-600">{errors.name?.message}</p>
+            )}
+          </div>
+
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              {...register("email", {
+                required: "Email Address is required",
+              })}
+              className="input input-bordered w-full max-w-xs"
+            />
+            {errors.email && (
+              <p className="text-red-600">{errors.email?.message}</p>
+            )}
+          </div>
+
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              {...register("password", {
+                required: "Password is required",
+                minLength: {
+                  value: 6,
+                  message: "Password must be 6 characters longer",
+                },
+                pattern: {
+                  value: /(?=.*[A-Z])(?=.*[!@#$&*])/,
+                  message:
+                    "Ensure password has at least one uppercase letter and one special character",
+                },
+              })}
+              className="input input-bordered w-full max-w-xs"
+            />
+            <label className="label">
+              <span className="label-text">Forget Password?</span>
+            </label>
+            {errors.password && (
+              <p className="text-red-600">{errors.password?.message}</p>
+            )}
+          </div>
+
+          {/* Default Value Start*/}
+
+          <input
+            className=" hidden"
+            defaultValue="learner"
+            {...register("role")}
+          />
+
+          {/* Default Value End */}
+
+          <input
+            className="btn btn-accent w-full"
+            value="Sign Up"
+            type="submit"
+          />
+        </form>
+        <p>
+          Already have an account{" "}
+          <Link className="text-primary" to="/login">
+            Please Login
+          </Link>
+        </p>
+        <div className="divider">OR</div>
+        <SocialLogin></SocialLogin>
+      </div>
+    </div>
+  );
+};
+export default SignUp;
--- e-school-client/src/Routes/Routes/Routes.jsx
+++ e-school-client/src/Routes/Routes/Routes.jsx
@@ -1,7 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import ErrorPage from "../../Pages/ErrorPage/ErrorPage";
-import Login from "../../Pages/Login/Login";
 import FAQPage from "../../Pages/FAQ/FAQPage";
+import Login from "../../Pages/Login/Login";
+import SignUp from "../../Pages/SignUp/SignUp";
 
 const router = createBrowserRouter([
   {
@@ -12,6 +13,10 @@ const router = createBrowserRouter([
       {
         path: "/login",
         element: <Login></Login>,
+      },
+      {
+        path: "/signup",
+        element: <SignUp></SignUp>,
       },
       {
         path: "/faq",
