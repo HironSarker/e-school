9e17ad158a40f84bbfaec07c2cabd3beace54f0d
unknown <hironsarker0005@gmail.com>
2024-12-27 16:20:31 +0600

    added Modal Component

--- e-school-client/src/main.jsx
+++ e-school-client/src/main.jsx
@@ -1,12 +1,18 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import ReactDOM from "react-dom/client";
 import { Toaster } from "react-hot-toast";
 import { RouterProvider } from "react-router-dom";
+import AuthProvider from "./Contexts/AuthProvider";
 import "./index.css";
 import router from "./Routes/Routes/Routes";
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <>
-    <Toaster></Toaster>
-    <RouterProvider router={router}></RouterProvider>
-  </>
+  <AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <Toaster></Toaster>
+      <RouterProvider router={router}></RouterProvider>
+    </QueryClientProvider>
+  </AuthProvider>
 );
