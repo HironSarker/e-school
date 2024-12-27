e055ad0da23bca5501036825b2f3449283029fa3
unknown <hironsarker0005@gmail.com>
2024-12-27 15:28:55 +0600

    added Routes Component

--- e-school-client/src/main.jsx
+++ e-school-client/src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import ReactDOM from "react-dom/client";
+import { Toaster } from "react-hot-toast";
+import { RouterProvider } from "react-router-dom";
+import "./index.css";
+import router from "./Routes/Routes/Routes";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <>
+    <Toaster></Toaster>
+    <RouterProvider router={router}></RouterProvider>
+  </>
+);
