fa15aecfa55f26fe38cba92c52aa6597615bc947
unknown <hironsarker0005@gmail.com>
2024-12-28 18:38:30 +0600

    added PrivateRoute Component

---
+++ e-school-client/src/Routes/PrivateRoute/PrivateRoute.jsx
@@ -0,0 +1,19 @@
+import { Navigate, useLocation } from "react-router-dom";
+import useAuth from "../../Hooks/useAuth/useAuth";
+
+const PrivateRoute = ({ children }) => {
+  const { user, loading } = useAuth();
+  const location = useLocation();
+
+  if (loading) {
+    return <span className="loading loading-spinner text-neutral"></span>;
+  }
+
+  if (user) {
+    return children;
+  }
+
+  return <Navigate to="/login" state={{ from: location }}></Navigate>;
+};
+
+export default PrivateRoute;
